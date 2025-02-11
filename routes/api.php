5d687ca53f636bb9ba1dfb869b58df0f9cafced8
Shruti Ladani <shruti.ladani@kalkani.in>
2025-02-11 11:08:50 +0530

    11/2/25 3rd video

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -9,6 +10,9 @@ use Illuminate\Http\Request;
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
+Route::get('/tickets', function(){
+    return Ticket::all();
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
