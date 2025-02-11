69944b0ddcdeaf1c1e39d8a7aa2819be6ebecb67
Shruti Ladani <shruti.ladani@kalkani.in>
2025-02-11 17:20:04 +0530

    11/2/25 8th video

--- app/Http/Resources/V1/TicketResource.php
+++ app/Http/Resources/V1/TicketResource.php
@@ -20,7 +20,10 @@ class TicketResource extends JsonResource
             'id' => $this->id,
             'attributes' => [
                 'title' => $this->title,
-                'description' => $this->description ,
+                'description' => $this->when(
+                    $request->routeIs('tickets.show'),
+                    $this->description
+                ),
                 'status' => $this->status,
                 'createdAt' => $this->created_at,
                 'updatedAt' => $this->updated_at
@@ -36,7 +39,9 @@ class TicketResource extends JsonResource
                     ]
                 ]
             ],
-
+            'includes' => [
+                new UserResource($this->user)
+            ],
             'links' => [
                 ['self' => route('tickets.show',  ['ticket' => $this->id])]
             ]
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UserController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
@@ -8,6 +9,8 @@ use App\Models\Ticket;
 
 
 Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
