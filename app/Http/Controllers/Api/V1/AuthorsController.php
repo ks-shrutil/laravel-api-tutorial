c07a194394cce3ed23252501be5db5304e13feba
Shruti Ladani <shruti.ladani@kalkani.in>
2025-02-12 17:55:52 +0530

    12/2/25 video-12

--- app/Http/Controllers/Api/V1/AuthorsController.php
+++ app/Http/Controllers/Api/V1/AuthorsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Http\Filters\V1\AuthorFilter;
 use App\Http\Requests\Api\V1\StoreUserRequest;
 use App\Http\Requests\Api\V1\UpdateUserRequest;
 use App\Http\Resources\V1\UserResource;
@@ -14,12 +15,10 @@ class AuthorsController extends ApiController
      *
      * @return void
      */
-    public function index()
+    public function index(AuthorFilter $filters)
     {
-        if ($this->include('tickets')) {
-            return UserResource::collection(User::with('tickets')->paginate());
-        }
-        return UserResource::collection(User::paginate());
+       
+        return UserResource::collection(User::filter($filters)->paginate());
     }
 
 
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Http\Filters\V1\QueryFilter;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -52,5 +54,9 @@ class User extends Authenticatable
     public function tickets(): HasMany
     {
         return $this->hasMany(Ticket::class);
+    }
+
+    public function scopeFilter(Builder $builder, QueryFilter $filters){
+        return $filters->apply($builder);
     }
 }
