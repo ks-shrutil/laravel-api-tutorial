add68d112af3ff1b76868d71c03db4295c4afaec
Shruti Ladani <shruti.ladani@kalkani.in>
2025-02-13 12:06:28 +0530

    13/2/25 video-15

--- app/Http/Controllers/Api/V1/TicketController.php
+++ app/Http/Controllers/Api/V1/TicketController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\ReplaceTicketRequest;
 use App\Http\Requests\Api\V1\UpdateTicketRequest;
 use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
@@ -43,7 +44,7 @@ class TicketController extends ApiController
             'user_id' => $request->input('data.relationships.author.data.id')
         ];
 
-        return Ticket::create($model);
+        return new TicketResource(Ticket::create($model));
     }
 
 
@@ -64,11 +65,28 @@ class TicketController extends ApiController
 
 
 
-    public function update(UpdateTicketRequest $request, Ticket $ticket)
-    {
-        //
-    }
+    public function update(UpdateTicketRequest $request, Ticket $ticket) {}
 
+    public function replace(ReplaceTicketRequest $request, $ticket_id)
+    {
+        try {
+            $ticket = Ticket::findOrFail($ticket_id);
+
+
+            $model = [
+                'title' => $request->input('data.attributes.title'),
+                'description' => $request->input('data.attributes.description'),
+                'status' => $request->input('data.attributes.status'),
+                'user_id' => $request->input('data.relationships.author.data.id')
+            ];
+
+            $ticket->update($model);
+
+            return new TicketResource($ticket);
+        } catch (ModelNotFoundException $exception) {
+            return $this->error('Ticket cannot be found.', 404);
+        }
+    }
 
 
     public function destroy($ticket_id)
---
+++ app/Http/Requests/Api/V1/ReplaceTicketRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Api\V1;
+
+use Illuminate\Foundation\Http\FormRequest;
+use PhpParser\Node\Expr\FuncCall;
+
+class ReplaceTicketRequest extends FormRequest
+{
+   
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $rules = [
+            'data.attributes.title' => 'required|string',
+            'data.attributes.description' => 'required|string',
+            'data.attributes.status' => 'required|string|in:A,C,H,X',
+            'data.relationships.author.data.id' => 'required|integer'
+        ];
+
+        if($this->routeIs('tickets.store')){
+            $rules['data.relationships.author.data.id'] = 'required|integer';
+        }
+
+        return $rules;
+    }
+
+    public function messages()
+    {
+        return[
+            'data.attributes.status' => 'The data.attributes.status value is invalid. Please use A, C, H, or X.'
+        ];
+    }
+}
